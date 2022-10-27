af5d315bd9c0cd069a1de38889616170e8043e73
rakeshliparefms2 <107123522+rakeshliparefms2@users.noreply.github.com>
2022-10-27 17:52:50 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,14 @@
+s = '190'
+print(int(s))  # str to int
+print(float(s))  # to float
+print(complex(s))  # to complex
+print(bool('190'))  # to boolean
+print(list(s))  # to list
+print(tuple(s))  # to tuple
+print(set(s))
+##print(bytearray(s))
+print(bin(int(s)))  # to binary
+print(bytes(int(s)))   # to bytes
+print(bytearray(int(s)))  # to bytearray
+print(oct(int(s)))  # to octal
+print(hex(int(s)))  # to hexadecimal
